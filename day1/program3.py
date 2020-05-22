bab79ae9c1b7af18eaa31bef72ec6267dbea1ef4
Pooja Jain <53304816+Pooja1826@users.noreply.github.com>
2020-05-22 19:34:52 +0530

    Create program3.py

---
+++ day1/program3.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+num1 = num1+num2
+num2= num1-num2
+num1= num1-num2
+print("After Swapping")
+print("Value of num1:",num1)
+print("Value of num2:",num2)
